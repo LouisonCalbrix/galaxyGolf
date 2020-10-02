dd09f76e611a82c983ffa8aedddb9569dc427c7c
LouisonCalbrix <34002086+LouisonCalbrix@users.noreply.github.com>
2020-10-02 16:20:36 +0200

    js file, no actual code yet only comments

---
+++ galaxy-golf.js
@@ -0,0 +1,14 @@
+// A golfing game taking place in outer space
+// date: October 2020
+// author: Louison Calbrix
+
+let Ball = function(pos) {
+    // attributes: position, velocity, hitbox
+    // methods: update
+}
+
+let Level = function(posStart, posGoal) {
+    // attributes: goal, ball
+    // goal = {x: xGoal, y: yGoal}
+    // methods: update
+}
