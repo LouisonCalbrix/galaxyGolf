15cc1bc9bc9ef5686864b721e2290c50c6c43a1b
LouisonCalbrix <34002086+LouisonCalbrix@users.noreply.github.com>
2020-10-28 11:08:32 +0100

    comments for Rectangle constructor and prototype

--- galaxy-golf.js
+++ galaxy-golf.js
@@ -2,13 +2,26 @@
 // date: October 2020
 // author: Louison Calbrix
 
-const defaultSize = 10;
-const defaultRadius = Math.round(defaultSize / Math.sqrt(Math.PI));
-const minForce = 40;
-const maxForce = 130;
 const fps = 80;
 const lvlWidth = 500;
 const lvlHeight = 500;
+
+/* Rectangle
+ * Represent the geometric shape of a rectangle. Top and bottom sides are 
+ * parallel to the x-axis. Rectangle constructor must be called with the 
+ * following arguments:
+ *  - pos: an array containing two numbers [x, y] which are coordinates for its 
+ *  upper-left corner. 
+ *  - width: a single number representing its width
+ *  - height: a single number representing its height
+ *  Interface of a rectangle object:
+ *  - collide: function
+ *  - middle (get/set)
+ *  - left (get/set)
+ *  - right (get/set)
+ *  - top (get/set)
+ *  - bottom (get/set)
+ */
 
 let Rectangle = function(pos, width, height) {
     this.pos = pos;
@@ -16,6 +29,7 @@ let Rectangle = function(pos, width, height) {
     this.height = height;
 }
 
+// middle (get/set): coordinates for the middle of the rectangle
 Object.defineProperty(Rectangle.prototype, 'middle', {
     get: function() { return [this.pos[0] - this.width / 2, this.pos[1] - this.height / 2]; },
     set: function(val) {
@@ -24,26 +38,35 @@ Object.defineProperty(Rectangle.prototype, 'middle', {
     }
 });
 
+// left (get/set): x coordinate for the left side of the rectangle
 Object.defineProperty(Rectangle.prototype, 'left', {
     get: function() { return this.pos[0]; },
     set: function(val) { this.pos[0] = val; }
 });
 
+// right (get/set): x coordinate for the right side of the rectangle
 Object.defineProperty(Rectangle.prototype, 'right', {
     get: function() { return this.pos[0] + this.width; },
     set: function(val) { this.pos[0] = val - this.width; }
 });
 
+// top (get/set): y coordinate for the top side of the rectangle
 Object.defineProperty(Rectangle.prototype, 'top', {
     get: function() { return this.pos[1]; },
     set: function(val) { this.pos[1] = val; }
 });
 
+// bottom (get/set): y coordinate for the bottom side of the rectangle
 Object.defineProperty(Rectangle.prototype, 'bottom', {
     get: function() { return this.pos[1] + this.height; },
     set: function(val) { this.pos[1] = val - this.height; }
 });
 
+
+/* Return true if two rectangles are intersecting, false otherwise. collide
+ * function must be called with the following argument:
+ *  -rect2: the second rectangle object
+ */
 Rectangle.prototype.collide = function(rect2) {
     return (this.left > rect2.left && this.left < rect2.right ||
         this.right > rect2.left && this.right < rect2.right) &&
@@ -51,31 +74,48 @@ Rectangle.prototype.collide = function(rect2) {
         this.bottom > rect2.top && this.bottom < rect2.bottom);
 }
 
-let Circle = function([x, y], size) {
+/* Circle
+ *
+ */
+
+// default size and radius for the golf ball
+const ballSize = 10;
+const ballRadius = Math.round(ballSize / Math.sqrt(Math.PI));
+// default size and radius for the goal
+const goalSize = 5;
+const goalRadius = Math.round(goalSize / Math.sqrt(Math.PI));
+
+let Circle = function([x, y], radius, hitSize) {
     this.pos = [x, y];
-    this.size = size;
-    this.hitbox = new Rectangle([x - size/2, y - size/2], size, size);
+    this.size = hitSize;
+    this.hitbox = new Rectangle([x - hitSize/2, y - hitSize/2], hitSize, hitSize);
 }
 
-let Goal = function(pos) {
-    Circle.call(this, pos, defaultSize);
-}
-
-let GolfBall = function(pos) {
-    Circle.call(this, pos, defaultSize);
+/* Golfball
+ *
+ */
+let Golfball = function(pos) {
+    Circle.call(this, pos, ballRadius, ballSize);
     this.vel = [0, 0];
 }
 
-GolfBall.prototype.update = function() {
+Golfball.prototype.update = function() {
     this.pos = this.pos.map((axis, index) => axis + this.vel[index]);
     this.hitbox.middle = this.pos;
 }
 
+/* Level
+ *
+ */
+
+const minForce = 40;
+const maxForce = 130;
+
 let Level = function(posStart, posGoal) {
     this.posStart = posStart;
     this.force = minForce;
-    this.ball = new GolfBall(posStart);
-    this.goal = new Goal(posGoal);
+    this.ball = new Golfball(posStart);
+    this.goal = new Circle(posGoal, goalRadius, goalSize);
 }
 
 Level.prototype.setForce = function(force) {
@@ -91,7 +131,7 @@ Level.prototype.update = function() {
     this.ball.update();
     if (this.ball.pos[0] < -2*this.ball.size || this.ball.pos[0] > lvlWidth + 2*this.ball.size ||
         this.ball.pos[1] < -2*this.ball.size || this.ball.pos[1] > lvlHeight + 2*this.ball.size)
-        this.ball = new GolfBall(this.posStart);
+        this.ball = new Golfball(this.posStart);
     if (this.ball.hitbox.collide(this.goal.hitbox)) {
         this.ball.vel = [0, 0];
         console.log('winner!!!');
@@ -121,7 +161,7 @@ let drawLevel = function(level) {
     // draw ball
     ctx.fillStyle = '#000';
     ctx.beginPath();
-    ctx.arc(level.ball.pos[0], level.ball.pos[1], defaultRadius, 0, 2*Math.PI);
+    ctx.arc(level.ball.pos[0], level.ball.pos[1], ballRadius, 0, 2*Math.PI);
     ctx.fill()
     // draw force
     ctx.lineWidth = 1;
@@ -133,7 +173,7 @@ let drawLevel = function(level) {
     ctx.lineWidth = 1;
     ctx.strokeStyle = '#F00';
     ctx.beginPath();
-    ctx.arc(level.goal.pos[0], level.goal.pos[1], defaultRadius, 0, 2*Math.PI);
+    ctx.arc(level.goal.pos[0], level.goal.pos[1], ballRadius, 0, 2*Math.PI);
     ctx.fill();
     ctx.stroke();
 }
